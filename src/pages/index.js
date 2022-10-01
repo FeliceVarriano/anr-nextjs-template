ae708d732738b7337538d56f05382c9a235b49ee
Felice Varriano <felice.varriano@gmail.com>
2022-10-01 14:46:48 -0400

    architecture of template more or less completed. Content to be updated.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,16 @@
-import React from 'react';
-import { ThemeProvider } from 'theme-ui';
-import { StickyProvider } from 'contexts/app/app.provider';
-import theme from 'theme';
-import SEO from 'components/seo';
-import Layout from 'components/layout';
-import Banner from 'sections/banner';
-import KeyFeature from 'sections/key-feature';
-import CoreFeature from 'sections/core-feature';
-import Feature from 'sections/feature';
-import PartnerSection from 'sections/partner';
-import WorkFlow from 'sections/workflow';
-import TestimonialCard from 'sections/testimonial';
-import SecurePayment from 'sections/secure-payment';
-import Package from 'sections/package';
-import Faq from 'sections/faq';
-import AboutUs from 'sections/about-us';
-import Gallery from 'sections/gallery';
+import React from "react";
+import { ThemeProvider } from "theme-ui";
+import { StickyProvider } from "contexts/app/app.provider";
+import theme from "theme";
+import SEO from "components/seo";
+import Layout from "components/layout";
+import Banner from "sections/banner";
+import TestimonialCard from "sections/testimonial";
+import Faq from "sections/faq";
+import AboutUs from "sections/about-us";
+import Gallery from "sections/gallery";
+import Pricing from "components/pricing";
+import OurProcess from "sections/our-process";
 
 export default function IndexPage() {
   return (
@@ -24,16 +19,12 @@ export default function IndexPage() {
         <Layout>
           <SEO title="Startup Landing 004" />
           <Banner />
-          <KeyFeature />
-          <CoreFeature />
-          <Feature />
-          <PartnerSection />
-          <WorkFlow />
-          <TestimonialCard />
-          <SecurePayment />
-          <Package />
-          <Faq />
           <AboutUs />
+          <OurProcess />
+          <TestimonialCard />
+          <Gallery />
+          <Pricing />
+          <Faq />
         </Layout>
       </StickyProvider>
     </ThemeProvider>
